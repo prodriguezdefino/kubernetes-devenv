8f27700ac8ce3d62e0dfbb1b614afa4250abe6e4
pablo rodriguez defino <prodriguezdefino@gmail.com>
2017-06-19 10:31:16 -0700

    improved help messages

--- devenv.sh
+++ devenv.sh
@@ -36,6 +36,15 @@ case "$1" in
     source ./commands/undeploy.sh $2
     ;;
   *)
-    echo "Usage: ${GREEN}devenv {setup|start|stop|gui|status|deploy-local <service-name> <image> <port>|undeploy <service-name>|...}${NC}"
+    echo "Usage:"
+    echo "  ${GREEN}devenv <command>${NC}"
+    echo "Commands:"
+    echo "  ${GREEN}setup${NC}                                       => install the development environment with all its dependencies on this machine."
+    echo "  ${GREEN}start${NC}                                       => boot up the base minimun infrastructure needed to run the different components of a project as containers."
+    echo "  ${GREEN}stop${NC}                                        => stops the development environment."
+    echo "  ${GREEN}gui${NC}                                         => opens up the kubernetes dashboard on the default browser."
+    echo "  ${GREEN}status${NC}                                      => prints the status of all devenv components."
+    echo "  ${GREEN}deploy-local <service-name> <image> <port>${NC}  => deploys a service in the local devenv from the container image provided with the indicated port."
+    echo "  ${GREEN}undeploy <service-name>${NC}                     => undeploy a service on the local devenv."
     exit 1
 esac
