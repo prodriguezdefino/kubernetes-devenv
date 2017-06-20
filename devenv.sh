2851e82ecca558e111b61a0e0774fd5f1be7139c
pablo rodriguez defino <prodriguezdefino@gmail.com>
2017-06-19 19:05:28 -0700

    changing setup to allow execution from arbitrary folders.

--- devenv.sh
+++ devenv.sh
@@ -1,50 +1,69 @@
 #!/usr/bin/env bash
 
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
-
 case "$1" in
   setup)
     source ./setup/setup-devenv.sh
     ;;
   start)
-    source ./commands/start.sh
+    source $KBNTS_DEVENV_DIR/commands/start.sh
     ;;
   stop)
-    source ./commands/stop.sh
+    source $KBNTS_DEVENV_DIR/commands/stop.sh
     ;;
   gui)
-    source ./commands/dashboard.sh
+    source $KBNTS_DEVENV_DIR/commands/dashboard.sh
     ;;
   status)
-    source ./commands/status.sh
+    source $KBNTS_DEVENV_DIR/commands/status.sh
     ;;
   deploy)
     if test "$#" -ne 4; then
       echo "Illegal number of parameters"
       echo "<service-name> <image> <port> are the expected parameters"
-      exit 1
+    else
+      source $KBNTS_DEVENV_DIR/commands/deploy-local.sh $2 $3 $4
     fi
-    source ./commands/deploy-local.sh $2 $3 $4
     ;;
   undeploy)
     if test "$#" -ne 2; then
       echo "Illegal number of parameters"
       echo "<service-name> is the expected parameter"
-      exit 1
+    else
+      source $KBNTS_DEVENV_DIR/commands/undeploy.sh $2
     fi
-    source ./commands/undeploy.sh $2
     ;;
   *)
     echo "Usage:"
-    echo "  ${GREEN}devenv <command>${NC}"
+    $GREEN
+    echo "  devenv <command>"
+    $NC
     echo "Commands:"
-    echo "  ${GREEN}setup${NC}                                       => install the development environment with all its dependencies on this machine."
-    echo "  ${GREEN}start${NC}                                       => boot up the base minimun infrastructure needed to run the different components of a project as containers."
-    echo "  ${GREEN}stop${NC}                                        => stops the development environment."
-    echo "  ${GREEN}gui${NC}                                         => opens up the kubernetes dashboard on the default browser."
-    echo "  ${GREEN}status${NC}                                      => prints the status of all devenv components."
-    echo "  ${GREEN}deploy-local <service-name> <image> <port>${NC}  => deploys a service in the local devenv from the container image provided with the indicated port."
-    echo "  ${GREEN}undeploy <service-name>${NC}                     => undeploy a service on the local devenv."
-    exit 1
+    $GREEN
+    echo "  setup"
+    $NC
+    echo "    install the development environment with all its dependencies on this machine."
+    $GREEN
+    echo "  start"
+    $NC
+    echo "    boot up the bare minimun infrastructure needed to run the different components of a project as containers."
+    $GREEN
+    echo "  stop"
+    $NC
+    echo "    stops the development environment."
+    $GREEN
+    echo "  gui"
+    $NC
+    echo "    opens up the kubernetes dashboard on the default browser."
+    $GREEN
+    echo "  status"
+    $NC
+    echo "    prints the status of all devenv components."
+    $GREEN
+    echo "  deploy-local <service-name> <image> <port>"
+    $NC
+    echo "    deploys a service in the local devenv from the container image provided with the indicated port."
+    $GREEN
+    echo "  undeploy <service-name>"
+    $NC
+    echo "    undeploy a service on the local devenv."
 esac
--- setup/setup-devenv.sh
+++ setup/setup-devenv.sh
@@ -2,16 +2,20 @@
 
 function checkInstallBrewPackage(){
   local package=$1
-  test $(brew ls --versions $package | grep -c $package) -eq 0 && brew install $package
+  test $(brew ls --versions $package | grep -c $package) -eq 0 && \
+  brew install $package
 }
 
 function checkInstallCaskPackage(){
   local package=$1
-  test $(brew cask ls --versions $package | grep -c $package) -eq 0 && brew cask install $package
+  test $(brew cask ls --versions $package | grep -c $package) -eq 0 && \
+  brew cask install $package
 }
 
-echo "${GREEN}checking local devenv dependencies..."
-echo "*************************************${NC}"
+tput setaf 2
+echo "checking local devenv dependencies..."
+echo "*************************************"
+tput sgr0
 echo " "
 echo "cheking homebrew installation..."
 [ ! -f "`which brew`" ] && source ./setup/install-homebrew.sh
@@ -29,9 +33,14 @@ echo "cheking kubernetes local cluster installation..."
 checkInstallCaskPackage "minikube"
 echo " "
 
-echo "setting up an alias for devenv command..."
+echo "setting up an aliases for devenv command..."
 # get current directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# add some coloring to scripts
+echo "export GREEN='tput setaf 2'" >> ~/.bash_profile
+echo "export NC='tput sgr0'" >> ~/.bash_profile
+# add alias for kubernetes devenv directory
+echo "export KBNTS_DEVENV_DIR=$DIR/.." >> ~/.bash_profile
 # add alias for the dev env
 echo "alias devenv='source $DIR/../devenv.sh'" >> ~/.bash_profile
 # persist the alias on current session
