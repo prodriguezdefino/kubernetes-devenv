5dd6bacdc8937c4511009dc8a65bbeec8e658860
pablo rodriguez defino <prodriguezdefino@gmail.com>
2017-06-23 13:22:15 -0700

    remote image deployment added

---
+++ commands/deploy-remote-image.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+function retrieveLocalUrl(){
+  $GREEN
+  echo "service deployed at:"
+  $NC
+  minikube service $1 --url
+  echo " "
+}
+
+$GREEN
+echo "deploying service..."
+echo "********************"
+$NC
+kubectl run $1 --image=$2 --port=$3 --image-pull-policy=Always && \
+kubectl expose deployment $1 --type=NodePort && \
+retrieveLocalUrl $1
+echo " "
